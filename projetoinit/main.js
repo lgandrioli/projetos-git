5720e56457ef3b8d94e1bfd012ada69a3ef40577
lgandrioli <74163919+lgandrioli@users.noreply.github.com>
2021-05-29 15:10:39 -0300

    inclusão do delete

--- projetoinit/main.js
+++ projetoinit/main.js
@@ -1,10 +1,8 @@
-
-
 /*$(document).ready(function (){
 
     $("#add-task").click(function(){
         alert('Adicionando nova tarefa');
-    });*/
+    });
     
   
     let buttonAddTask = document.getElementById("add-task");
@@ -62,3 +60,58 @@
                       </tr>`
                       table.innerHTML += template
    })*/
+function deleterow(event) {
+  if (!event.target.classList.contains("deleteBtn")) return;
+  const botdelete = event.target;
+  botdelete.closest("tr").remove();
+}
+function addtask() {
+  let dateNow = new Date();
+
+  // referência do elemento "descrição (input)"
+  let taskDescriptionInput = document.getElementById("task-description");
+
+  // titulo
+  let taskTitleInput = document.getElementById("task-title");
+
+  // tabela de atividades
+  let taskTable = document.getElementById("task-table");
+
+  // elemento <tbody> presente dentro da tabela de atividades
+  let taskTableTBody = taskTable.querySelector("tbody");
+
+  let index = 1;
+  let title = taskTitleInput.value;
+  let description = taskDescriptionInput.value;
+
+  let day = dateNow.getDate();
+  let month = ("" + (dateNow.getMonth() + 1)).padStart(2, "0");
+  let year = dateNow.getFullYear();
+  let hour = dateNow.getHours();
+  let minute = dateNow.getMinutes();
+  let seconds = dateNow.getSeconds();
+
+  let dateStr = `${day}/${month}/${year} ${hour}:${minute}:${seconds}`;
+
+  // nova linha a ser inserida apartir da ação de click
+  let template = `<tr>
+                           <td scope="row">${index}</td>
+                           <td>${title}</td>
+                           <td>${description}</td>
+                           <td>${dateStr}</td>
+                           <td><button type="button" class="deleteBtn btn btn-danger">delete</button></td>
+                       </tr>`;
+  taskTableTBody.insertAdjacentHTML("beforeend", template);
+
+  //botao de deletar
+
+  taskTable.addEventListener("click", deleterow);
+}
+
+let buttonAddTask = document.getElementById("add-task");
+
+// attach on element button
+buttonAddTask.addEventListener("click",addtask);
+
+
+//<td type="button" class="deleteBtn" id="deletebutton">delete</td>
